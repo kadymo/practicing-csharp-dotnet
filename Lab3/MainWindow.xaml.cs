80dd8a2163be8ecf830569fdccc837414c397ab1
Kádymo <santanakadymo@gmail.com>
2025-07-23 23:54:50 -0300

    Initializing and testing first UI element

---
+++ Lab3/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Lab3;
+
+public partial class MainWindow : Window
+{
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+    private void RevelationButton_Click(object sender, RoutedEventArgs e)
+    {
+        RevelationText.Text = "O Corinthians é o maior clube do Brasil.";
+        RevelationButton.IsEnabled = false;
+        RevelationButton.Content = "Revelado!";
+    }
+}
