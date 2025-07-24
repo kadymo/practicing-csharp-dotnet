72d8e39012b5b2be12c132fe155c2a96a9180ae2
Kádymo <santanakadymo@gmail.com>
2025-07-24 09:26:52 -0300

    Created authenticated page

---
+++ Lab3/Authenticated.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Lab3;
+
+public partial class Authenticated : Page
+{
+    public Authenticated(UserViewModel userViewModel)
+    {
+        InitializeComponent();
+    }
+}
