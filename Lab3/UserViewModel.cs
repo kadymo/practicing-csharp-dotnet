025f9d6e598a4dd260f92e99657946064007a904
Kádymo <santanakadymo@gmail.com>
2025-07-24 09:27:41 -0300

    Created model for user data transition

---
+++ Lab3/UserViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Lab3;
+
+public class UserViewModel : INotifyPropertyChanged
+{
+    private string _username;
+
+    public string Username
+    {
+        get { return _username; }
+        set { _username = value; OnPropertyChanged(); }
+    }
+
+    private string _email;
+
+    public string Email
+    {
+        get { return _email; }
+        set { _email = value; OnPropertyChanged(); }
+    }
+
+    private string _password;
+    public string Password
+    {
+        get { return _password; }
+        set { _password = value; OnPropertyChanged(); }
+    }
+
+    private bool _acceptTerms;
+    public bool AcceptTerms
+    {
+        get { return _acceptTerms; }
+        set { _acceptTerms = value; OnPropertyChanged(); }
+    }
+    
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
