a0ea42d573c3312912a6db384fd9a116f163f61a
Kádymo <santanakadymo@gmail.com>
2025-08-02 11:44:18 -0300

    FEAT: Manager for keyboard input

---
+++ Lab4/Lab4/Services/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using Windows.System;
+
+namespace Lab4.Services;
+
+public class InputManager
+{
+    private readonly HashSet<VirtualKey> _pressedKeys = new HashSet<VirtualKey>();
+    
+    public void KeyUp(VirtualKey key)
+    {
+        _pressedKeys.Remove(key);
+    }
+    
+    public void KeyDown(VirtualKey key)
+    {
+        _pressedKeys.Add(key);
+    }
+
+    public bool isKeyPressed(VirtualKey key)
+    {
+        return _pressedKeys.Contains(key);
+    }
+}
