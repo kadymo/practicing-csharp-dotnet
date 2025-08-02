f715209ca4076eaa1562a643a7b8e32a07ee819f
Kádymo <santanakadymo@gmail.com>
2025-08-02 11:45:25 -0300

    FEAT: Mapping args for Collision Event

---
+++ Lab4/Lab4/Models/CollisionEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab4.Models;
+
+public class CollisionEventArgs
+{
+    public GameObject ProjectileGameObject { get; set; }
+    public GameObject EnemyGameObject { get; set; }
+
+    public CollisionEventArgs(GameObject projectileGameObject, GameObject enemyGameObject)
+    {
+        ProjectileGameObject = projectileGameObject;
+        EnemyGameObject = enemyGameObject;
+    }
+}
