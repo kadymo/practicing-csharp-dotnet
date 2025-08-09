1e8d89f4f87dfcc022a557c07896f0e9da84982b
Kádymo <santanakadymo@gmail.com>
2025-08-09 00:33:54 -0300

    FEAT: Separating notification message from generics and params

--- Lab5/Lab5/MainPage.xaml.cs
+++ Lab5/Lab5/MainPage.xaml.cs
@@ -1,5 +1,10 @@
+using Windows.UI.Notifications;
 using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.WinUI.UI.Controls;
+using Lab5.Enums;
+using Lab5.Models;
 using Lab5.ViewModels;
+using Microsoft.UI;
 
 namespace Lab5;
 
@@ -16,11 +21,14 @@ public sealed partial class MainPage : Page
         ViewModel = new MainViewModel(messenger);
         this.DataContext = ViewModel;
         
-        messenger.Register<string>(this, (_contentLoaded, message ) => ShowNotification(message));
+        messenger.Register<NotificationMessage>(this, (_contentLoaded, message ) =>
+        {
+            ShowNotification(message);
+        });
     }
 
-    private void ShowNotification(String message)
+    private void ShowNotification(NotificationMessage message)
     {
-        Notification.Show(message);
+        Notification.Show(message.Text, message.Duration);
     }
 }
